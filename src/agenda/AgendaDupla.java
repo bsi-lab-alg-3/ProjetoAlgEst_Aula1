1c35cffd37a97a1ae1ca70eb8101469d9b3ed155
Romulo Fagundes Cantanhede <romulo.fagundes@gmail.com>
2016-02-22 19:37:54 -0300

    - Iniciado a estrutura da lista duplamente encadeada.

---
+++ src/agenda/AgendaDupla.java
@@ -0,0 +1,16 @@
+package agenda;
+
+public class AgendaDupla {
+
+	private Contato primeiro;
+	private Contato auxiliar;
+	
+	public void adicionarComeco(Contato contato){
+		
+	}
+	
+	public void adicionarFim(Contato contato){
+		
+	}
+	
+}
--- src/agenda/Contato.java
+++ src/agenda/Contato.java
@@ -3,7 +3,9 @@ package agenda;
 public class Contato {
 
 	private Pessoa pessoa;
+	
 	private Contato proximo;
+	private Contato anterior;
 	
 	public Pessoa getPessoa() {
 		return pessoa;
@@ -16,6 +18,12 @@ public class Contato {
 	}
 	public void setProximo(Contato proximo) {
 		this.proximo = proximo;
+	}
+	public Contato getAnterior() {
+		return anterior;
+	}
+	public void setAnterior(Contato anterior) {
+		this.anterior = anterior;
 	}
 	
 	
